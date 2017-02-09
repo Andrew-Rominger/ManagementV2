512ca4ac97e3383dc9c6c2436b53f5f9c491244c
Unknown <andrewrominger@outlook.com>
2017-02-08 23:43:41 -0800

    Nav Menu Start

--- app/src/main/java/com/management/HomeScreen.java
+++ app/src/main/java/com/management/HomeScreen.java
@@ -2,12 +2,17 @@ package com.management;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity
+{
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        toolbar = (Toolbar) findViewById(R.id.mainToolbar);
     }
 }
