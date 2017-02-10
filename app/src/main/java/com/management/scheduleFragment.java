18499a0a9c86e18d9973300bca79e26ea67da718
WesBanghart <Wbang98@gmail.com>
2017-02-09 18:50:22 -0800

    fixed some layout issues

--- app/src/main/java/com/management/scheduleFragment.java
+++ app/src/main/java/com/management/scheduleFragment.java
@@ -1,6 +1,11 @@
 package com.management;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by wesleybanghart on 2/9/17.
@@ -8,4 +13,15 @@ import android.app.Fragment;
 
 public class scheduleFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.schedule_fragment_layout, container, false );
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
