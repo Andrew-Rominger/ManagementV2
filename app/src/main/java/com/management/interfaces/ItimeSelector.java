dfeaa53f23df1bcf8badefa4e89336c003e28584
Unknown <andrewrominger@outlook.com>
2017-02-11 13:02:27 -0800

    missed file

--- app/src/main/java/com/management/interfaces/ItimeSelector.java
+++ app/src/main/java/com/management/interfaces/ItimeSelector.java
@@ -1,8 +1,12 @@
 package com.management.interfaces;
 
+import java.util.Calendar;
+
 /**
  * Created by Andre on 2/11/2017.
  */
 
-public interface ItimeSelector {
+public interface ItimeSelector
+{
+    void passTime(Calendar c);
 }
